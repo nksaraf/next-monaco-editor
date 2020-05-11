0893130329bc831e418a2789d1eef534693102df
Nikhil Saraf <nsaraf98@gmail.com>
2020-05-10 22:20:08 -0400

    delete unnecessary files

--- scripts/copy-workers.ts
+++
@@ -1,9 +0,0 @@
-// Deno.copyFileSync('./src/plugins/prettier.monaco.worker.ts', './plugins/prettier.monaco.worker.ts');
-const text = new TextDecoder().decode(Deno.readFileSync('./next-monaco-editor/plugins/prettier.monaco.worker.ts'));
-
-Deno.writeFile('./plugins/prettier.monaco.worker.ts', new TextEncoder().encode(text.replace('../worker', "next-monaco-editor/worker").replace('../api', "next-monaco-editor/api")));
-
-// Deno.copyFileSync('./src/plugins/prettier.monaco.worker.ts', './plugins/prettier.monaco.worker.ts');
-const css = new TextDecoder().decode(Deno.readFileSync('./next-monaco-editor/css/monaco.css'));
-
-Deno.writeFile('./dist/monaco.css', new TextEncoder().encode(css));
